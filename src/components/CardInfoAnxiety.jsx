bc8a4349ee2823c9f5a49b130038bde9e5655326
fxsym <fsyamsudin24@gmail.com>
2025-05-30 01:32:48 +0700

    Fixing info about anxiety section

--- src/components/CardInfoAnxiety.jsx
+++ src/components/CardInfoAnxiety.jsx
@@ -3,14 +3,11 @@ import AnxietyIlus from "../assets/images/AnxietyIlus.png"
 export const CardInfoAnxiety = ({ title, desc }) => {
     return (
         <>
-            <div className="flex flex-col items-center bg-slate-100/40 backdrop-blur-sm p-6 m-4 my-8 rounded-2xl shadow-black shadow-2xl sm:mx-12 md:mx-14 lg:mx-18 xl:mx-24">
+            <div className="flex flex-col items-center bg-slate-100/40 backdrop-blur-sm p-6 m-4 my-8 rounded-2xl shadow-black shadow-2xl lg:w-1/3">
                 <img src={AnxietyIlus} alt="Ilustrasi Anxiety" className="w-80" />
                 <div className="flex flex-col items-center mt-4">
                     <h1 className="font-bold text-slate-800 text-xl">{title}</h1>
                     <p className="text-center text-slate-800 mt-2">{desc}</p>
-                </div>
-                <div>
-                    <button className="text-center text-white/80 mt-2 bg-slate-900 py-2 px-8 rounded-full">Next</button>
                 </div>
             </div>
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
 import { Navbar } from "../components/navbar"
-import CenterContainer from "../layout/CenterContainer"
-import AnxietyIlus from "../assets/images/AnxietyIlus.png"
 import { CardInfoAnxiety } from "../components/CardInfoAnxiety"
 
 
@@ -9,9 +7,12 @@ export const Home = () => {
         <>
             {/* <div className="flex flex-col items-center border shadow-lg fixed inset-0 -z-10"></div> */}
             <Navbar />
-            <CardInfoAnxiety title={"Apa itu anxiety?"} desc={"Menurut para ahli bahwa kecemasan (anxiety) adalah salah satu gejala kejiwaan yang negatif disebabkan oleh tekanan emosional atau fisik sehingga menimbulkan perasaan atau emosi negatif pada seseorang seperti rasa gelisah dan rasa takut Rhamadian, D., & Jumrotul'Aqobah, Q. (2022)."} />
-            <CardInfoAnxiety title={"Faktor Penyebab Anxiety"} desc={"Menurut para ahli bahwa kecemasan"} />
-
+            <div className="flex flex-col lg:flex-row gap-1 px-2 sm:px-12 md:px-14 lg:px-18 xl:px-24">
+                <CardInfoAnxiety title={"Apa itu anxiety?"} desc={"Menurut para ahli bahwa kecemasan (anxiety) adalah salah satu gejala kejiwaan yang negatif disebabkan oleh tekanan emosional atau fisik sehingga menimbulkan perasaan atau emosi negatif pada seseorang seperti rasa gelisah dan rasa takut Rhamadian, D., & Jumrotul'Aqobah, Q. (2022)."} />
+                <CardInfoAnxiety title={"Faktor Penyebab Anxiety"} desc={"Menurut para ahli bahwa kecemasan"} />
+                <CardInfoAnxiety title={"Anxiety Level"} desc={"Menurut para ahli bahwa kecemasanMenurut para ahli bahwa kecemasanMenurut para ahli bahwa kecemasanMenurut para ahli bahwa kecemasanMenurut para ahli bahwa kecemasan"} />
+            </div>
+            
         </>
     )
 }
