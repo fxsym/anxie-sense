ce7a99baaef854bf3e58c61692058cf50759a49b
fxsym <fsyamsudin24@gmail.com>
2025-05-30 01:18:01 +0700

    Fixing navbar, make info about anxieety to card components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import Logo from "../assets/images/Logo.png"
 export const Navbar = () =>{
     return (
         <>
-            <div className="flex items-center justify-center bg-slate-800 w-full p-2">
-                <img src={Logo} alt="" className="w-[10%]"/>
+            <div className="flex items-center  bg-slate-800 w-full px-4 h-20 sm:px-12 md:px-14 lg:px-18 xl:px-24">
+                <img src={Logo} alt="" className="max-h-16"/>
                 <p className="text-[#A3CFFB]">AnxieSense</p>
             </div>
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,16 @@
 import { Navbar } from "../components/navbar"
 import CenterContainer from "../layout/CenterContainer"
 import AnxietyIlus from "../assets/images/AnxietyIlus.png"
+import { CardInfoAnxiety } from "../components/CardInfoAnxiety"
 
 
 export const Home = () => {
     return (
         <>
-            <div className="flex flex-col items-center bg-slate-800/20 backdrop-brightness-80 border shadow-lg fixed inset-0 -z-10"></div>
+            {/* <div className="flex flex-col items-center border shadow-lg fixed inset-0 -z-10"></div> */}
             <Navbar />
-            <div className="flex flex-col items-center bg-slate-800/40 backdrop-blur-sm border border-white/10 p-6 m-4 my-8 rounded-2xl shadow-lg">
-                <img src={AnxietyIlus} alt="Ilustrasi Anxiety" className="w-80" />
-                <div className="flex flex-col items-center mt-4">
-                    <h1 className="font-bold text-white text-xl">Apa itu anxiety?</h1>
-                    <p className="text-center text-white/80 mt-2">Menurut para ahli bahwa kecemasan (anxiety) adalah salah satu gejala kejiwaan yang negatif disebabkan oleh tekanan emosional atau fisik sehingga menimbulkan perasaan atau emosi negatif pada seseorang seperti rasa gelisah dan rasa takut Rhamadian, D., & Jumrotul'Aqobah, Q. (2022).</p>
-                </div>
-                <div>
-                    <button className="text-center text-white/80 mt-2 bg-slate-900 py-2 px-8 rounded-full">Next</button>
-                </div>
-            </div>
+            <CardInfoAnxiety title={"Apa itu anxiety?"} desc={"Menurut para ahli bahwa kecemasan (anxiety) adalah salah satu gejala kejiwaan yang negatif disebabkan oleh tekanan emosional atau fisik sehingga menimbulkan perasaan atau emosi negatif pada seseorang seperti rasa gelisah dan rasa takut Rhamadian, D., & Jumrotul'Aqobah, Q. (2022)."} />
+            <CardInfoAnxiety title={"Faktor Penyebab Anxiety"} desc={"Menurut para ahli bahwa kecemasan"} />
 
         </>
     )
