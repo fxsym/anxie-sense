c1067397fff0b66067b82bfa9958a7b8d92d7475
fxsym <fsyamsudin24@gmail.com>
2025-05-31 16:08:50 +0700

    Add state for form

--- src/api/getPrediction.js
+++ src/api/getPrediction.js
@@ -1,28 +1,17 @@
-function getPrediction() {
+import axios from 'axios';
+
+export const getPrediction = async (data) => {
     const inputData = {
-        "Sleep Hours": 6,
-        "Physical Activity": 2,
-        "Caffeine Intake": 100,
-        "Alcohol Consumption": 0,
-        "Stress Level": 7,
-        "Heart Rate": 80,
-        "Breathing Rate": 18,
-        "Sweating Level": 3
+        "Sleep Hours": Number(data.sleepHours),
+        "Physical Activity": Number(data.physicalActifity),
+        "Caffeine Intake": Number(data.caffeine),
+        "Alcohol Consumption": Number(data.alcohol),
+        "Stress Level": Number(data.stress),
+        "Heart Rate": Number(data.heartRate),
+        "Breathing Rate": Number(data.breathingRate),
+        "Sweating Level": Number(data.sweatingLevel)
     };
 
-    fetch('http://127.0.0.1:5000/predict', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(inputData),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Predicted Anxiety Level:', data.predicted_anxiety_level);
-        // Tampilkan hasil di UI
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
+    const response = await axios.post('http://127.0.0.1:5000/predict', inputData);
+    return response.data; // pastikan API mengembalikan `predicted_anxiety_level`
+};
--- src/components/InputFormPrediction.jsx
+++ src/components/InputFormPrediction.jsx
@@ -1,9 +1,10 @@
-export const InputFormPrediction = ({info, description, placeHolder}) => {
+export const InputFormPrediction = ({info, description, placeHolder, setAnswer}) => {
+
     return (
         <div className="w-100 flex flex-col items-start">
             <p className="text-md font-bold mb-6">{info}</p>
             <p className="text-justify mb-4">{description}</p>
-            <input type="text" placeholder={placeHolder} className="w-full border py-2 px-4 rounded-2xl"/>
+            <input type="text" placeholder={placeHolder} className="w-full border py-2 px-4 rounded-2xl" onChange={(e) => setAnswer(e.target.value)}/>
         </div>
     )
 }
