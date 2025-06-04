1ff3034c5480d7f3f19fb4523e70586002a4badf
fsyamsudin24@gmail.com <eprianggii@gmail.com>
2025-06-04 20:42:40 +0700

    Add hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
-import Logo from "../assets/images/Logo.png"
+import Logo from "../assets/images/LogoTypograph.png"
 
 export const Navbar = () =>{
     return (
         <>
-            <div className="flex items-center  bg-slate-800 w-full px-4 h-20 sm:px-12 md:px-14 lg:px-18 xl:px-24">
+            <div className="flex items-center w-full px-4 h-20 sm:px-12 md:px-14 lg:px-18 xl:px-24">
                 <img src={Logo} alt="" className="max-h-16"/>
-                <p className="text-[#A3CFFB]">AnxieSense</p>
             </div>
         </>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,18 @@ export const Home = () => {
         <>
             {/* <div className="flex flex-col items-center border shadow-lg fixed inset-0 -z-10"></div> */}
             <Navbar />
+            <div className="bg-sky-200 min-h-150 flex flex-col justify-center items-center p-4">
+                <div className="mb-8">
+                    <p className="text-4xl font-[Nunito] font-bold text-center">Kenali Tingkat Anxietymu Sekarang!</p>
+                </div>
+                <div className="mb-10">
+                    <p className="text-2xl font-[Nunito] font-bold text-center leading-7">Ukur level kecemasanmu secara instan dan rahasia. Mulailah perjalanan menuju kesehatan mental yang lebih baik.</p>
+                </div>
+                <div>
+                    <button className="bg-emerald-100 py-2 px-8 rounded-full cursor-pointer">Get Started</button>
+                </div>
+            </div>
+
             <div className="flex flex-col lg:flex-row gap-1 px-2 sm:px-12 md:px-14 lg:px-18 xl:px-24">
                 <CardInfoAnxiety title={"Apa itu anxiety?"} desc={"Menurut para ahli bahwa kecemasan (anxiety) adalah salah satu gejala kejiwaan yang negatif disebabkan oleh tekanan emosional atau fisik sehingga menimbulkan perasaan atau emosi negatif pada seseorang seperti rasa gelisah dan rasa takut Rhamadian, D., & Jumrotul'Aqobah, Q. (2022)."} />
                 <CardInfoAnxiety title={"Faktor Penyebab Anxiety"} desc={"Menurut para ahli bahwa kecemasan"} />
