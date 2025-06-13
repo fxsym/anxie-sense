439875e9f9dc932e0bdfcb05169f9ec64525bf2e
fxsym <fsyamsudin24@gmail.com>
2025-06-13 21:10:17 +0700

    Add validation to form prediction for user can only input number

--- src/components/InputFormPrediction.jsx
+++ src/components/InputFormPrediction.jsx
@@ -4,7 +4,7 @@ export const InputFormPrediction = ({info, description, placeHolder, setAnswer, 
         <div className="w-70 lg:w-140 flex flex-col items-start" ref={targetRef}>
             <p className="text-md font-bold mb-6">{info}</p>
             <p className="text-justify mb-4">{description}</p>
-            <input type="text" placeholder={placeHolder} className="w-full border py-2 px-4 rounded-2xl" onChange={(e) => setAnswer(e.target.value)}/>
+            <input type="number" placeholder={placeHolder} className="w-full border py-2 px-4 rounded-2xl" onChange={(e) => setAnswer(e.target.value)}/>
         </div>
     )
 }
